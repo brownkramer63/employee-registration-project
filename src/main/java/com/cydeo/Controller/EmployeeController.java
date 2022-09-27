cc90e79458c0bc600ca037068ed873926584a785
Kramer Brown <brownkramer63@gmail.com>
2022-09-27 12:11:35 -0500

    employeeRegistrationProject

--- src/main/java/com/cydeo/Controller/EmployeeController.java
+++ src/main/java/com/cydeo/Controller/EmployeeController.java
@@ -2,14 +2,24 @@ package com.cydeo.Controller;
 
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller //will auto create bean
 @RequestMapping("/employee")//all methods will start with this endpoint
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
 
     @GetMapping("/register")//so when we put in employee register in url it will return the return below
     public String createEmployee(Model model){
@@ -22,6 +32,16 @@ public class EmployeeController {
         return "employee/employee-create";
     }
 
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") Employee employee){//will capture objects sent from ui
+
+        employeeService.saveEmployee(employee);
+        return "employee/employee-list";
+
+
+
+
+    }
 
 
 }
