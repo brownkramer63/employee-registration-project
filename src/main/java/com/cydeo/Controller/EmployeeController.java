828c3b9f4f3d3030f7a0ce60c2ee91f19dc76810
Kramer Brown <brownkramer63@gmail.com>
2022-09-27 10:53:15 -0500

    employeeRegistrationProject

--- src/main/java/com/cydeo/Controller/EmployeeController.java
+++ src/main/java/com/cydeo/Controller/EmployeeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller //will auto create bean
-@RequestMapping("employee")//all methods will start with this endpoint
+@RequestMapping("/employee")//all methods will start with this endpoint
 public class EmployeeController {
 
     @GetMapping("/register")//so when we put in employee register in url it will return the return below
